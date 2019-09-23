09746cb3d4b911065759f0e541aba354c0cfcf0b
abgehtdiepost <david.gruber@rb-werbeagentur.at>
2019-09-23 21:07:46 +0200

    dazugehoerige Main-Klasse

---
+++ Desktop/Schule/3. Klasse/dgruber_hue1/src/hue1/Main.java
@@ -0,0 +1,22 @@
+package hue1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EratosthenesPrimeSieve pS = new EratosthenesPrimeSieve(120);
+    }
+    
+}
