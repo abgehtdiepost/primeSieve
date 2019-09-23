a2ac86edfd312b0568a35265d23be56b0b298fa6
abgehtdiepost <david.gruber@rb-werbeagentur.at>
2019-09-23 20:18:55 +0200

    Testversion

---
+++ Desktop/Schule/3. Klasse/dgruber_hue1/src/hue1/EratosthenesPrimeSieve.java
@@ -0,0 +1,48 @@
+package hue1;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class EratosthenesPrimeSieve implements PrimeSieve {
+    public int prime;
+    public ArrayList<Integer> noPrimes;
+    public ArrayList<Integer> primes;
+    
+    public EratosthenesPrimeSieve (int prime) {
+        prime = this.prime;
+        noPrimes  = new ArrayList<Integer>();
+        primes = new ArrayList<Integer>();
+        isPrime(prime);
+        printPrimes();
+    }
+    
+    
+    @Override
+    public boolean isPrime(int p) {
+        
+        
+        return true;
+    }
+
+    @Override
+    public void printPrimes() {
+        for (int i = 0; i < primes.size(); i++) {
+            System.out.println(primes.get(i));
+        }
+        System.out.println("ab geht die post");
+    }
+    
+    public void getPrimes (int p) {
+        
+    }
+    
+}
